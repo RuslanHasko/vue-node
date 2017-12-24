18c0ea4e2af304146e5cba9dcbc2fbab9f0e36f0
Ruslan Hasko <ruslan.hasko@kindgeek.com>
2017-12-24 14:45:04 +0200

    Added MongoDB Atlas connection

--- services/ToDoListAPI/config/index.js
+++ services/ToDoListAPI/config/index.js
@@ -1,5 +1,11 @@
 module.exports = {
   secret: 'todolistsecret',
   session: { session: false },
-  database: 'mongodb://127.0.0.1:27017/todolist'
+  database: 'mongodb://RuslanHasko:MaRk_mongodb_1494@' +
+  'ruslanhasko-cluster1-shard-00-00-uflk4.mongodb.net:27017,' +
+  'ruslanhasko-cluster1-shard-00-01-uflk4.mongodb.net:27017,' +
+  'ruslanhasko-cluster1-shard-00-02-uflk4.mongodb.net:27017/todolist' +
+  '?replicaSet=RuslanHasko-Cluster1-shard-0' +
+  '&ssl=true' +
+  '&authSource=admin'
 }
--- services/index.js
+++ services/index.js
@@ -4,7 +4,7 @@ const log = require('color-log');
 const http = require('http'),
       ToDoListAPI = require('@ToDoListAPI'),
       ToDoListServer = http.Server(ToDoListAPI),
-      ToDoListPORT = process.env.PORT || 3001,
+      ToDoListPORT = process.env.PORT || 3002,
       LOCAL = '0.0.0.0';
 
 ToDoListServer.listen(ToDoListPORT, LOCAL, () => log.info(`\nToDoListAPI running on ${ToDoListPORT}`));
